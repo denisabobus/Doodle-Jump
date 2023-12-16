a2e807f0f46960c0d7996419d353509e9680210c
denisabobus <152689249+denisabobus@users.noreply.github.com>
2023-12-16 13:33:57 +0300

    Add files via upload

--- scripts/app.py
+++ scripts/app.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 from scripts.game import Game
+from scripts.functions import load_image
 class App:
     def __init__(self) :
         self.display_size = (480, 720)
@@ -12,7 +13,7 @@ class App:
         self.game = Game()
 
         pygame.display.set_caption('Doodle Jump')
-        pygame.display.set_icon(pygame.image.load(os.path.join("assets","icons","icon.ico")))
+        pygame.display.set_icon(load_image("assets","icons","icon.ico"))
 
     def handle_events(self):
         for event in pygame.event.get():
@@ -29,8 +30,8 @@ class App:
 
     def run(self):
         while self.running:
-            self.hundle_events()
-            self.upgrate()
+            self.handle_events()
+            self.update()
             self.render()
 
             self.clock.tick(self.maxFPS)
